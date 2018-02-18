b21dfcf38fc209ce7381ae7256fa895b15a08c90
Callum Quigley <super-callum@hotmail.com>
2018-02-18 22:23:47 +0000

    added basic layout of component

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../../src/header.css';
+import '../../src/about.css';
 
 class About extends Component {
     
@@ -13,8 +14,26 @@ class About extends Component {
   render() 
   {
     return (
-       //initial commit comment
-        <h1>About</h1>
+      <div className="AboutSection">
+      <h1>About Me</h1>
+      <div className="Centerted-section">
+      <div className="row">
+      <div className="col s8">
+      <p>background background background</p>
+      <p> experience experience experience</p>
+      <p>proof of work proof of work proof of work </p>
+      <div className="buttonSection">
+      <div className="button"> <NavLink to="/portfolio"> Portfolio</NavLink>    </div>
+      <div className="button"> <NavLink to="/contact"> Contact</NavLink>    </div></div>
+      </div>
+      <div className="col s4">
+      <div className="image-container">
+      <div className="image-container-image"><img src="../../public/graduationPicture.jpg"></img></div>
+      <div className="image-container-text">Graduation, June 2017</div>
+      </div>
+      </div>
+      </div>
+      </div></div>
     );
   }
 }
