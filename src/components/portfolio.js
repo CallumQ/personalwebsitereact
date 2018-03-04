624b2d105788732092817b56686ddce5473cf47b
Callum Quigley <super-callum@hotmail.com>
2018-03-04 18:22:32 +0000

    Added pagination, kind of works, but requires two clicks and then increments the pageNo twice

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,48 +1,107 @@
 import React, { Component } from 'react';
-import '../../src/header.css';
+import '../../src/portfolio.css';
 import Project from './project';
 
 const projects = [
   {
     name :"Project One",
-    description : "My personal website Written using React.js.",
+    summary : "A simple web browser written in C# which performs HTTP requests.",
+    description : "this is a description this is a description this is a description this is a description this is a description this is a description ",
+    previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
+    technology:["C","JAVA"],
+    url: "/this/will/point/to/a/endpoint",
+  },{
+    name :"Project One",
+    summary : "A simple web browser written in C# which performs HTTP requests.",
+    description : "this is a description this is a description this is a description this is a description this is a description this is a description ",
+    previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
+    technology:["C","JAVA"],
+    url: "/this/will/point/to/a/endpoint",
+  },{
+    name :"Project One",
+    summary : "A simple web browser written in C# which performs HTTP requests.",
+    description : "this is a description this is a description this is a description this is a description this is a description this is a description ",
+    previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
+    technology:["C","JAVA"],
+    url: "/this/will/point/to/a/endpoint",
+  },{
+    name :"Project One",
+    summary : "A simple web browser written in C# which performs HTTP requests.",
+    description : "this is a description this is a description this is a description this is a description this is a description this is a description ",
+    previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
+    technology:["C","JAVA"],
+    url: "/this/will/point/to/a/endpoint",
+  },{
+    name :"Project One",
+    summary : "A simple web browser written in C# which performs HTTP requests.",
+    description : "this is a description this is a description this is a description this is a description this is a description this is a description ",
+    previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
+    technology:["C","JAVA"],
+    url: "/this/will/point/to/a/endpoint",
+  },{
+    name :"Project One",
+    summary : "A simple web browser written in C# which performs HTTP requests.",
+    description : "this is a description this is a description this is a description this is a description this is a description this is a description ",
     previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
     technology:["C","JAVA"],
     url: "/this/will/point/to/a/endpoint",
   },
   {
     name :"Project two",
-    description : "A simple web browser written in C# which performs HTTP requests.",
+    summary : "A simple web browser written in C# which performs HTTP requests.",
+    description : "this is a description",
     previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
     technology:["C","CPLUSPLUS"],
     url: "/this/will/point/to/a/endpoint",
   },
   {
     name :"Project three",
+    summary : "A simple web browser written in C# which performs HTTP requests.",
     description : "this is a description",
     previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
     technology:["REACT","CSS","HTML5","JAVASCRIPT","MONGODB"],
     url: "/this/will/point/to/a/endpoint",
   },
-  {
-    name :"Project four",
-    description : "this is a description",
-    previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
-    technology:["NODE","PYTHON","HTML5"],
-    url: "/this/will/point/to/a/endpoint",
-  },
+
 ]
+let NoOfItems = 0;
+let pageNo = 0;
+let itemsOnPage = 8;
 class Portfolio extends Component {  
   
   constructor(){
     super();
     this.state = {
       projects,
+      NoOfItems,
+      itemsOnPage,
+      pageNo
     }
+    this.upPage = this.upPage.bind(this);
+    this.downPage = this.downPage.bind(this);
+  }
+
+  upPage(){
+    
+    this.setState({pageNo :pageNo++});
+  }
+
+  downPage(){
+
+    this.setState({pageNo : pageNo--});
   }
 
   componentDidMount() {
     document.title = "Portfolio - Callum Quigley";
+  }
+  createItems(){
+    let projectArray = [];
+    for (let i = this.state.itemsOnPage*this.state.pageNo; i <this.state.itemsOnPage; i++){
+      projectArray.push( <Project key={projects[i].name} properties={projects[i]}/>);
+
+    }
+    return projectArray;
+
   }
   
     render() 
@@ -54,11 +113,21 @@ class Portfolio extends Component {
             <div className="col s10">
               <h1>Projects</h1>
               <div className="row">
-                {this.state.projects.map(project => <Project key={project.name} properties={project}/>)}
-              </div>  
+              {this.createItems()}
+               
+              </div>
+              
             </div>
             <div className="col s1"></div>  
           </div>
+
+          <div className="row center-align fixed-bottom">
+              <ul class="pagination ">
+    <li class="disabled"><a href="#!" onClick={this.downPage}><i class="material-icons">chevron_left</i></a></li>
+    <li class="active"><a href="#!">{this.state.pageNo+1}</a></li>
+    <li class="waves-effect"><a href="#!" onClick={this.upPage}><i class="material-icons">chevron_right</i></a></li>
+  </ul>
+              </div>  
         </div>
       );
     }
--- src/components/project.js
+++ src/components/project.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import '../../src/project.css';
 import TechnologyItem from './technologyItem';
-
+import { NavLink } from 'react-router-dom';
 class Project extends Component {  
 
   render() 
   {
     return (
       <div className="col s3">
-        <div className="card large hoverable">
+        <div className="card large hoverable sticky-action">
           <div className="card-content">
             <span className="card-title activator grey-text text-darken-4">{this.props.properties.name}</span>
           </div>
@@ -18,25 +18,25 @@ class Project extends Component {
           </div>
           <div className="divider"></div>
           <div className="card-content">
-            {this.props.properties.description}
+            {this.props.properties.summary}
           </div>
-          <div class="card-action">
-            <div class="left-section valign-wrapper">
+          <div className="card-action">
+            <div className="left-section valign-wrapper">
               <div className="language-text">Languages: </div>
-              {this.props.properties.technology.map(tech => <TechnologyItem item={tech} stringName={tech}/>)}
+              {this.props.properties.technology.map(tech => <TechnologyItem key={tech}item={tech} stringName={tech}/>)}
             </div>
-            <div class="right-section valign-wrapper">
-              <a href="/">More info</a>
+            <div className="right-section valign-wrapper ">
+              <NavLink to={"portfolio/"+this.props.properties.url}> Expand</NavLink>
             </div> 
           </div>
-          <div class="card-reveal">
-            <span class="card-title grey-text text-darken-4">
-              Card Title
-              <i class="material-icons right">
+          <div className="card-reveal">
+            <span className="card-title grey-text text-darken-4">
+            {this.props.properties.name}
+              <i className="material-icons right">
                 close
               </i>
             </span>
-            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            <p> {this.props.properties.description}</p>
           </div>
         </div>
       </div>
