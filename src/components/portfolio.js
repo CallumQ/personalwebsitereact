723cced0248551073fc8f913eda66915963956e4
Callum Quigley <super-callum@hotmail.com>
2018-03-04 01:50:27 +0000

    Added tooltips and string readable technologies, plus some code cleaning

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -7,42 +7,42 @@ const projects = [
     name :"Project One",
     description : "My personal website Written using React.js.",
     previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
-    technology:["C"],
+    technology:["C","JAVA"],
     url: "/this/will/point/to/a/endpoint",
   },
   {
     name :"Project two",
     description : "A simple web browser written in C# which performs HTTP requests.",
     previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
-    technology:["C"],
+    technology:["C","CPLUSPLUS"],
     url: "/this/will/point/to/a/endpoint",
   },
   {
     name :"Project three",
     description : "this is a description",
     previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
-    technology:["js","react","css","HTML","ECMA6","Mongo"],
+    technology:["REACT","CSS","HTML5","JAVASCRIPT","MONGODB"],
     url: "/this/will/point/to/a/endpoint",
   },
   {
     name :"Project four",
     description : "this is a description",
     previewImage: "https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h556",
-    technology:["js","react","css","HTML","ECMA6","Mongo"],
+    technology:["NODE","PYTHON","HTML5"],
     url: "/this/will/point/to/a/endpoint",
   },
 ]
 class Portfolio extends Component {  
+  
   constructor(){
     super();
     this.state = {
       projects,
     }
-  
   }
+
   componentDidMount() {
     document.title = "Portfolio - Callum Quigley";
-   
   }
   
     render() 
@@ -50,14 +50,14 @@ class Portfolio extends Component {
       return (
         <div className="project-container">
           <div className="row">
-          <div className="col s1"></div>
+            <div className="col s1"></div>
             <div className="col s10">
               <h1>Projects</h1>
-            <div className="row">
-              {this.state.projects.map(project => <Project key={project.name} properties={project}/>)}
-            </div>  
-          </div>
-          <div className="col s1"></div>  
+              <div className="row">
+                {this.state.projects.map(project => <Project key={project.name} properties={project}/>)}
+              </div>  
+            </div>
+            <div className="col s1"></div>  
           </div>
         </div>
       );
--- src/components/project.js
+++ src/components/project.js
@@ -3,46 +3,46 @@ import '../../src/project.css';
 import TechnologyItem from './technologyItem';
 
 class Project extends Component {  
-  constructor(props){
-    super(props);  
-  }
 
   render() 
   {
     return (
-      <div className="col s3"><div className="card large hoverable"><div className="card-content">
-        <span className="card-title activator grey-text text-darken-4">{this.props.properties.name}</span>
-      </div>
-      <div className="divider"></div>
-      <div className="card-image waves-effect waves-block waves-light">
-        <img className="activator" src={this.props.properties.previewImage} />
-      </div>
-      <div className="divider"></div>
-      <div className="card-content">
-        {this.props.properties.description}
-      </div>
-      <div class="card-action">
-        <div class="left-section valign-wrapper">
-          <div className="language-text">Languages: </div>
-          {this.props.properties.technology.map(tech => <TechnologyItem item={tech}/>)}</div>
-          <div class="right-section valign-wrapper">
-            <a href="#">More info</a>
-          </div> 
+      <div className="col s3">
+        <div className="card large hoverable">
+          <div className="card-content">
+            <span className="card-title activator grey-text text-darken-4">{this.props.properties.name}</span>
+          </div>
+          <div className="divider"></div>
+          <div className="card-image waves-effect waves-block waves-light">
+            <img className="activator" src={this.props.properties.previewImage} alt="project preview" />
+          </div>
+          <div className="divider"></div>
+          <div className="card-content">
+            {this.props.properties.description}
+          </div>
+          <div class="card-action">
+            <div class="left-section valign-wrapper">
+              <div className="language-text">Languages: </div>
+              {this.props.properties.technology.map(tech => <TechnologyItem item={tech} stringName={tech}/>)}
+            </div>
+            <div class="right-section valign-wrapper">
+              <a href="/">More info</a>
+            </div> 
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">
+              Card Title
+              <i class="material-icons right">
+                close
+              </i>
+            </span>
+            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          </div>
         </div>
-        <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4">
-          Card Title
-          <i class="material-icons right">
-            close
-          </i>
-        </span>
-        <p>Here is some more information about this product that is only revealed once clicked on.</p>
       </div>
-     </div>
-     </div>
       );
     }
-  
+    
 }
 
 export default Project;
