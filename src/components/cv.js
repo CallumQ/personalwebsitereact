0e89f2b69cf7ab6cdd62cbc5780946449e1c502b
Callum Quigley <super-callum@hotmail.com>
2018-04-22 20:56:55 +0100

    set up most of the template for the CV page

--- src/components/cv.js
+++ src/components/cv.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import '../../src/header.css';
-
+import '../../src/cv.css';
 class CV extends Component {
   
   componentDidMount() {
@@ -11,7 +11,56 @@ class CV extends Component {
   render() 
   {
     return (
-      <span>todo</span>
+      <div className="cv-container">
+        <div className="center-align">
+          <a href="#" className="button-link">
+            <div className="outer-button">
+              <div className="inner-button">
+                <p className="buttonText">Download CV</p>
+              </div> 
+            </div>
+          </a>
+        </div>
+        <div className="technology-experience center-align">
+          <div class="row"> 
+            <div class="col s3"></div>
+            <div class="col s6">
+              <div class="row left-align">
+                <div class="col s3 offer-seperator offer-header">Technologies / Skills</div>
+                <div class="col s1"></div>
+                <div class="col s4 offer-header">Experienced
+                  <ul className="skills-list">
+                    <li className="list-item-seperator">Multi-threaded applications</li>
+                  </ul>
+                </div>
+                <div class="col s4 offer-header">Some experience
+                  <ul className="skills-list">
+                    <li>Item 1</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col s3"></div>
+          </div>
+        </div>
+        <div className="related-experience center-align">
+          <div class="row"> 
+            <div class="col s3"></div>
+            <div class="col s6">
+              <div class="row left-align">
+                <div class="col s3 offer-seperator offer-header">Technologies / Skills</div>
+                <div class="col s1"></div>
+                <div class="col s8 offer-header">Some experience
+                  <ul className="skills-list">
+                    <li>Item 1</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col s3"></div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
