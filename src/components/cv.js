54d31164e15fb4b984bc3aa84568c8df09291ede
Callum Quigley <super-callum@hotmail.com>
2018-05-06 18:33:13 +0100

    Download cv button works as intended now, filler text also added

--- src/components/cv.js
+++ src/components/cv.js
@@ -22,13 +22,13 @@ class CV extends Component {
           </a>
         </div>
         <div className="technology-experience center-align">
-          <div class="row"> 
-            <div class="col s3"></div>
-            <div class="col s6">
-              <div class="row left-align">
-                <div class="col s3 offer-seperator offer-header">Technologies / Skills</div>
-                <div class="col s1"></div>
-                <div class="col s4 offer-header">Experienced
+          <div className="row"> 
+            <div className="col s3"></div>
+            <div className="col s6">
+              <div className="row left-align">
+                <div className="col s3 offer-seperator offer-header">Technologies / Skills</div>
+                <div className="col s1"></div>
+                <div className="col s4 offer-header">Experienced
                   <ul className="skills-list">
                     <li className="list-item-seperator">C#</li>
                     <li className="list-item-seperator">Java (JUnit 4)</li>
@@ -46,7 +46,7 @@ class CV extends Component {
                     <li className="list-item-seperator">Version Control (Git)</li>
                   </ul>
                 </div>
-                <div class="col s4 offer-header">Some experience
+                <div className="col s4 offer-header">Some experience
                   <ul className="skills-list">
                     <li>C</li>
                     <li>C++</li>
@@ -57,51 +57,56 @@ class CV extends Component {
                 </div>
               </div>
             </div>
-            <div class="col s3"></div>
+            <div className="col s3"></div>
           </div>
         </div>
         <div className="related-experience center-align">
-          <div class="row"> 
-            <div class="col s3"></div>
-            <div class="col s6">
-              <div class="row left-align">
-                <div class="col s3 offer-seperator offer-header">Relevant Experience</div>
-                <div class="col s1"></div>
-                <div class="col s8 offer-header">
+          <div className="row"> 
+            <div className="col s3"></div>
+            <div className="col s6">
+              <div className="row left-align">
+                <div className="col s3 offer-seperator offer-header">Relevant Experience</div>
+                <div className="col s1"></div>
+                <div className="col s8 offer-header">
                   <div className="offer-item">
                     <div className="offer-header">Lab Assistant, Heriot-Watt University</div>
                     <div className="offer-date">12th September 2016 - 28th November 2016</div>
-                    <div className="offer-details">I provided support to the new 1st year students on a course called praxis, which aims to teach the new students time management skills, study skills, methods of research, and report writing. I also helped teach them the basics of shell navigation and BASH scripting</div>
+                    <div className="offer-details">I provided support to the new 1st year students on a course called praxis, which aims to teach the new students time management skills, study skills, methods of research, and report writing. I also helped teach them the basics of shell navigation and BASH scripting.</div>
+                  </div>
+                  <div className="offer-item">
+                    <div className="offer-header">Programmer &amp; group liaison, Heriot-Watt Group Project</div>
+                    <div className="offer-date">12th September 2015 - 1st April 2016</div>
+                    <div className="offer-details">I was part of a group of 7 people who were tasked to create an online career pathfinder aimed at school leavers with the aim of going to university. My primary roles were to liaise between the project manager and the group, Gathering the data from all of the different universities via Python using Scrapy and also constructing the MySql database to supply the students with suitable suggestions.</div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="col s3"></div>
+            <div className="col s3"></div>
           </div>
         </div>
 
          <div className="technology-experience center-align">
-          <div class="row"> 
-            <div class="col s3"></div>
-            <div class="col s6">
-              <div class="row left-align">
-                <div class="col s3 offer-seperator offer-header">Education</div>
-                <div class="col s1"></div>
-                <div class="col s8 offer-header">
+          <div className="row"> 
+            <div className="col s3"></div>
+            <div className="col s6">
+              <div className="row left-align">
+                <div className="col s3 offer-seperator offer-header">Education</div>
+                <div className="col s1"></div>
+                <div className="col s8 offer-header">
                   <div className="offer-item">
                     <div className="offer-header">BSc (Hons) Computer Systems, Heriot-Watt University</div>
                     <div className="offer-date">12th September 2015 - 21st June 2017, 1st Class Honours</div>
-                    <div className="offer-details">some description</div>
+                    <div className="offer-details">Computer Systems is a course that offers an in depth look at programming including things such as multi-threaded programming, network topologies, artificial intelligence, methods of testing software, and language processors. It also includes project management, different methods of analyzing the problem situation, ensuring content is delivered in the best manner, and also the importance of the human element of organizations.</div>
                   </div>
                   <div className="offer-item">
-                    <div className="offer-header">BSc (Hons) Computer Systems, Heriot-Watt University</div>
-                    <div className="offer-date">12th September 2015 - 21st June 2017, 1st Class Honours</div>
-                    <div className="offer-details">some description</div>
+                    <div className="offer-header">Software Development HND, Forth Valley College Falkirk</div>
+                    <div className="offer-date">31 August 2012 - 30 June 2015</div>
+                    <div className="offer-details">While at Forth Valley College, I studied a broad spectrum of programming disciplines including Android app development, MySql databases, OOP (Object Orientated Programming), Web development, Data structures, and a large group project where I developed an online stock management system.</div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="col s3"></div>
+            <div className="col s3"></div>
           </div>
         </div>
       </div>
